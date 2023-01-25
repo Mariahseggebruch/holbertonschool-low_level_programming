6a142c16f8f38f4c7408b95c58ce5dfa58551954
Mariahseggebruch <mariagseggebruch@gmail.com>
2023-01-25 09:17:58 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+		//string initialisation
+	char Mystr [] = ""Programming is like building a multilingual puzzle/n";
+
+	puts(Mystr); //writing the string to stdout
+
+	return 0;
+}
