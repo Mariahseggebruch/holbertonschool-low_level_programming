706b3d84db2c0d83d298e96133a9e2f7717975a0
Mariahseggebruch <mariagseggebruch@gmail.com>
2023-02-02 12:02:44 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#include "holberton.h"
+
 /**
  * more_numbers - Prints the numbers 0-14 ten times.
  */
@@ -7,7 +9,7 @@ void more_numbers(void)
 {
 	int num, count;
 
-	for (count + 0; count <= 9; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
