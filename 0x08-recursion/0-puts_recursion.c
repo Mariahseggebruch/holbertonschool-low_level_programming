4319922b4230fc65677f4f611980b7ff9040b638
Mariahseggebruch <mariagseggebruch@gmail.com>
2023-02-15 11:38:57 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,13 @@
 * Return: Always 0
 */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 if (*s == '\0')
-_putchar('\n')
+_putchar('\n');
 else
-{_putchar(*s);
+{
+_putchar(*s);
 _puts_recursion(s + 1);
 }
 }
