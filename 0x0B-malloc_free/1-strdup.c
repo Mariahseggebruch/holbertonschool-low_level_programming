deebcbc26566b3803707144fba8d68416341bbdf
Mariahseggebruch <mariagseggebruch@gmail.com>
2023-02-20 17:39:26 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,22 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-/**
- * free_grid - use free function on previous grid
- * @grid: 2 dim. array to free
- * @height: input height (size)
- * Return: free'd grid
- */
-void free_grid(int **grid, int height)
-{
-	int i;
+#include <stdlib.h>
 
-	if (grid != NULL || height != 0)
-	{
-		for (i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
-		free(grid);
-	}
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ *           containing a copy of the string given as parameter.
+ * @str:string
+ *
+ * Return:char
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, l = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		l++;
+
+	s = malloc(sizeof(char) * (l + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		s[i] = str[i];
+
+	s[l] = '\0';
+
+	return (s);
 }
